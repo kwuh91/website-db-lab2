53f0862d1ad3213da32131b0c6a9909bf3d45c3e
kwuh <nkt.ochkin@gmail.com>
2024-01-02 13:08:19 +0300

    finished

--- djsite/coolsite/playground/models.py
+++ djsite/coolsite/playground/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class InputHistory(models.Model):
+    input_text = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.input_text
--- djsite/coolsite/playground/views.py
+++ djsite/coolsite/playground/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
-from django.db import connection, connections
-from .models import *
+from django.db import connection, connections, DatabaseError, IntegrityError, OperationalError
+from typing import Any
+from django.core.management.base import BaseCommand
+from .models import InputHistory
+from django.utils import timezone
 
 # Create your views here.
 
@@ -9,6 +12,8 @@ def index(request):
     return render(request, 'playground/authentication.html')
 
 def process_form(request):
+    error_msg = "No errors detected"
+    history_entries = InputHistory.objects.all()
     if request.method == 'POST':
         # Get the values from the form
         address = request.POST.get('address')
@@ -17,7 +22,7 @@ def process_form(request):
         pswd = request.POST.get('pswd')
         port = request.POST.get('port')
 
-        # Now you can use these values as needed (e.g., connect to the database)
+        # connecting to the database
         connection.settings_dict['HOST'] = address
         connection.settings_dict['NAME'] = db_name
         connection.settings_dict['USER'] = login
@@ -25,17 +30,31 @@ def process_form(request):
         connection.settings_dict['PORT'] = port
 
 
-        # For testing purposes, you can print the values
-        print(f"Address: {connection.settings_dict['HOST']}, Database: {connection.settings_dict['NAME']}, Login: {connection.settings_dict['USER']}, Password: {connection.settings_dict['PASSWORD']}, Port: {connection.settings_dict['PORT']}")
+        # For testing purposes, printing the values
+        # print(f"Address: {connection.settings_dict['HOST']}, Database: {connection.settings_dict['NAME']}, Login: {connection.settings_dict['USER']}, Password: {connection.settings_dict['PASSWORD']}, Port: {connection.settings_dict['PORT']}")
 
-        # Add your logic here (e.g., connect to the database)
-        # with connection.cursor() as cursor:
-        #     cursor.execute("SELECT * FROM world.city;")
-        #     for item in cursor:
-        #         print(*item)
 
-        # For now, return a simple response
-        # return HttpResponse("Form submitted successfully.")
-        return render(request, 'playground/workbench.html')
+        with connection.cursor() as tables:
+            tables.execute("SHOW FULL TABLES;")
+            return render(request, 'playground/workbench.html', {'data' : None, 'tables' : tables, 'error_msg' : error_msg, 'history_entries' : history_entries})
+        
     else:
-        return HttpResponse("Form submission failed.")
+        query = request.GET.get("qinput")
+        print(f"Query: {query}")
+        
+        # Add logic here
+
+        db_Info = connection.mysql_server_info
+        print("Connected to MySQL Server version ", db_Info)
+
+
+        with connection.cursor() as data, connection.cursor() as tables:
+            tables.execute("SHOW FULL TABLES;")
+            try:
+                data.execute(query)
+                InputHistory.objects.create(input_text=query, timestamp=timezone.now())
+                history_entries = InputHistory.objects.all()
+                return render(request, 'playground/workbench.html', {'data' : data, 'tables' : tables, 'error_msg' : error_msg, 'history_entries' : history_entries})
+            except Exception as e:
+                error_msg = str(e)
+                return render(request, 'playground/workbench.html', {'data' : None, 'tables' : tables, 'error_msg' : error_msg, 'history_entries' : history_entries})
